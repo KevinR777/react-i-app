c979967ecc27e6a323efb1d118e9aa2c9bff8150
KevinR777 <krod995@gmail.com>
2023-05-03 20:36:07 -0600

    Setting up contact page

---
+++ src/containers/contact/contact-container.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContactContainer = () => {
+    return <div>
+        Contact Container
+    </div>
+}
+
+export default ContactContainer
---
+++ src/pages/contact/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ContactContainer from '../../containers/contact/contact-container'
+
+const ContactPage = () => {
+    return <div id="contact-page">
+       <ContactContainer/>
+    </div>
+}
+
+export default ContactPage
